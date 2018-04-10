fff510c274f1894d9ffaecbc72e19b830031952c
kcollard2 <36429491+kcollard2@users.noreply.github.com>
2018-04-09 23:51:21 -0400

    New
    New

---
+++ SideScroller/Assets/Project/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public AudioSource music;
+
+    public Slider masterSlider;
+    public Slider musicSlider;
+
+    void Update ()
+    {
+        AudioListener.volume = masterSlider.value;
+        music.volume = musicSlider.value;
+    }
+}
